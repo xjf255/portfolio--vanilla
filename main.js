c641a110ae3b6647fdf6cf99f794e1956800f0da
xjf255 <juanfher.255@gmail.com>
2024-03-06 00:12:13 -0600

    add design

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 // import './style.css'
 
+const navItem = document.querySelectorAll('.li--item>a')
 const $presentationText = document.querySelectorAll('.profile__text')
-console.log($presentationText)
+const $template = document.getElementById('shape--projects').content
+const $sectionProjects = document.getElementById('section__projects')
+const $fragment = document.createDocumentFragment()
 
+document.addEventListener('click', e => {
+  navItem.forEach(item => {
+    const itemSelect = document.querySelector('.--active')
+    if (item === e.target && item !== itemSelect) {
+      itemSelect.classList.remove('--active')
+      itemSelect.classList.add('--default')
+      item.classList.add('--active')
+      item.classList.remove('--default')
+    }
+  })
+})
 
-const changeText = setTimeout(() =>{
-  $presentationText.forEach((txt) =>{
+const changeText = setTimeout(() => {
+  $presentationText.forEach((txt) => {
     txt.classList.toggle('text--hide')
   })
-},5000)
+}, 5000)
+
+fetch('projects.json')
+  .then(json => json.json())
+  .then(list => {
+    list['projects']?.map(el => {
+      $template.querySelector('.project__title').textContent = el.title
+      let $clone = document.importNode($template, true)
+      $fragment.appendChild($clone)
+    })
+    $sectionProjects.appendChild($fragment)
+  })
 
